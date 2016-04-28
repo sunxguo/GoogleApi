f454e7a8b86ada79999a7233754fd8051776da09
MonkeyKing1024 <MonkeyKing1024@gmail.com>
2016-04-28 15:26:24 +0800

    update

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->helper("base");
+	}
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index2()
+	{
+		// $this->load->view('welcome_message');
+		$url="https://maps.googleapis.com/maps/api/geocode/json";
+		$param=array(
+			'address'=>"1600+Amphitheatre+Parkway,+Mountain+View,+CA",
+			'key'=>"AIzaSyBSA5oOEb1bJ8zmlXG0ZLzLMNoocr-bvcQ"
+			);
+		$result=httpGet($url, $param);
+		print_r($result);
+	}
+	public function index(){
+		$requestUrl = 'google.com';
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch, CURLOPT_URL, $requestUrl);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC); //代理认证模式
+		curl_setopt($ch, CURLOPT_PROXY, "u46.vpntm.com:80"); //代理服务器地址
+		// curl_setopt($ch, CURLOPT_PROXYPORT, 80); //代理服务器端口
+		curl_setopt($ch, CURLOPT_PROXYUSERPWD, 'MonkeyKing:19910910Mk1024'); //http代理认证帐号，username:password的格式
+		curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
+		$file_contents = curl_exec($ch);
+		curl_close($ch);
+		echo $file_contents;
+	}
+}
