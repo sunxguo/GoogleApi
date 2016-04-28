13df7690be8cd5cc66541b5b01222c7cf7081223
MonkeyKing1024 <MonkeyKing1024@gmail.com>
2016-04-28 17:08:16 +0800

    update

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Api extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->helper("base");
